c423cafc88ea067bfaa6ea3f5c764a939d24d47d
MrLoles <pchoromanski281@gmail.com>
2023-12-11 01:00:48 +0100

    FIT-2 Added Theme with Material3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitfinder/themes/fitfinder_main_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: FitFinderProTheme.buildTheme(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
