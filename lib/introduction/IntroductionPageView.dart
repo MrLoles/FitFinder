6c9db8a5e8b07c9df6ea7c0536008f89bc3296f5
MrLoles <pchoromanski281@gmail.com>
2024-02-18 21:22:02 +0100

    FIT-1 - Finish Introduction screen

---
+++ lib/introduction/IntroductionPageView.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  PageController _controller = PageController();
+  bool onLastPage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        PageView(
+          controller: _controller,
+          onPageChanged: (index){
+            setState(() {
+              onLastPage = (index == 2);
+            });
+          },
+          children: [
+            TextContainer(
+              title: AppLocalizations.of(context)!.introduction1_1,
+              description: AppLocalizations.of(context)!.introduction1_2,
+              color: Colors.purple,
+            ),
+            TextContainer(
+              title: AppLocalizations.of(context)!.introduction2_1,
+              description: AppLocalizations.of(context)!.introduction2_2,
+              color: Colors.purpleAccent,
+            ),
+            TextContainer(
+              title: AppLocalizations.of(context)!.introduction3_1,
+              description: AppLocalizations.of(context)!.introduction3_2,
+              color: Colors.deepPurpleAccent,
+            ),
+          ],
+        ),
+        Container(
+            alignment: Alignment(0, 0.8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(2);
+                    },
+                    child: Text(AppLocalizations.of(context)!.skip)),
+                SmoothPageIndicator(controller: _controller, count: 3),
+                onLastPage?
+                GestureDetector(
+                    onTap: () {
+                      print("TODO");
+                    },
+                    child: Text(AppLocalizations.of(context)!.done)) :
+                GestureDetector(
+                    onTap: () {
+                      _controller.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn);
+                    },
+                    child: Text(AppLocalizations.of(context)!.next))
+              ],
+            ))
+      ]),
+    );
+  }
+}
+
+class TextContainer extends StatelessWidget {
+  final String title;
+  final String description;
+  final Color color;
+
+  const TextContainer({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SizedBox.expand(
+        child: Container(
+          color: color,
+          child: Center(
+            child: Card(
+              elevation: 20,
+              margin: EdgeInsets.all(20),
+              color: color.withAlpha(600),
+              child: ListTile(
+                title: Padding(
+                    padding: const EdgeInsets.only(bottom: 15.0),
+                    child: Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    )),
+                subtitle: Text(
+                  description,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:fitfinder/introduction/IntroductionPageView.dart';
 import 'package:fitfinder/themes/fitfinder_main_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'l10n/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,13 +12,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: FitFinderProTheme.buildTheme(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      supportedLocales: L10n.all,
+      locale: const Locale('pl'),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: OnBoardingScreen()
     );
   }
 }
@@ -54,6 +66,9 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              AppLocalizations.of(context)!.language
             ),
           ],
         ),
