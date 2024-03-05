48f3769b8e002032597aa873e97c612878837180
Patryk Choromański <pchoromanski281@gmail.com>
2024-03-05 15:01:42 +0100

    FIT-13 Fix naming

--- lib/general/LoadingSpinner.dart
+++ lib/general/LoadingSpinner.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CustomCircularProgressIndicator extends StatelessWidget {
+class LoadingSpinner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
