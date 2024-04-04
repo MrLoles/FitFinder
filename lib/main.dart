1a3f969b6f624cb608afd9778d2e202e20a82d1f
Patryk Choromański <pchoromanski281@gmail.com>
2024-04-05 01:53:38 +0200

    FIT-11 Implement mainscreen without API requests

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,33 @@ import 'l10n/l10n.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class MyApp2 extends StatelessWidget {
+  final List<String> items = List.generate(10, (index) => 'Element ${index + 1}');
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lista Przewijana'),
+        ),
+        body: PageView.builder(
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Center(
+              child: Text(
+                items[index],
+                style: TextStyle(fontSize: 24.0),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -21,6 +47,8 @@ class MyApp extends StatelessWidget {
       title: 'FitFinder',
       theme: FitFinderProTheme.buildTheme(),
       supportedLocales: L10n.all,
+      // locale: const Locale('en'),
+
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -68,6 +96,19 @@ class _StartState extends State<Start> {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: new AuthService().validateToken(token),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return LoadingSpinnerPage();
+        }
+        else {
+          // bool tokenLogin = snapshot.data ?? false; //TODO uncomment automatyczane logowanie
+          // return tokenLogin ? MainScreen() : StartPage();
+          return StartPage();
+        }
+      },
+    );
     return firstLaunch ? OnBoardingScreen() : StartPage();
   }
 }
--- lib/main_page/MainScreen.dart
+++ lib/main_page/MainScreen.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../general/Calendar.dart';
 
 class MainScreen extends StatelessWidget {
+  final List<Map<String, String>> _dataList = [
+    {
+      'nazwa': 'Firma A',
+      'miasto': 'Kraków',
+      'ulica': 'ul. Kawiarniana 12',
+      'hours': '8:00-22:00',
+      'image':
+          'https://st2.depositphotos.com/1017228/7108/i/450/depositphotos_71080145-stock-photo-gym-interior-with-equipment.jpg'
+    },
+    {
+      'nazwa': 'Firma B',
+      'miasto': 'Warszawa',
+      'ulica': 'ul. Handlowa 5',
+      'hours': '8:00-24:00',
+      'image':
+          'https://static.vecteezy.com/system/resources/thumbnails/026/781/389/small/gym-interior-background-of-dumbbells-on-rack-in-fitness-and-workout-room-photo.jpg'
+    },
+    {
+      'nazwa': 'Firma C',
+      'miasto': 'Gdańsk',
+      'ulica': 'ul. Portowa 8',
+      'hours': '6:00-22:00',
+      'image':
+          'https://img.freepik.com/premium-photo/powerful-fitness-gym-background_849761-28993.jpg'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +42,214 @@ class MainScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: CustomSidebar(),
+      body: ListView.builder(
+        itemCount: 1,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Text(
+                    "Twoje siłownie:",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                ),
+                PageViewBox(
+                    dataList: _dataList),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Twój dzisiejszy plan:",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardTraining(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Placeholder",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      Card(
+                        child: Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                // Kolor tła kontenera
+                                borderRadius: BorderRadius.circular(10),
+                                // Zaokrąglenie rogów kontenera
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.1),
+                                    // Kolor cienia
+                                    spreadRadius: 3,
+                                    // Rozprzestrzenienie cienia
+                                    blurRadius: 7,
+                                    // Rozmycie cienia
+                                    offset: Offset(0, 3), // Offset cienia
+                                  ),
+                                ],
+                              ),
+                              child: ListTile(
+                                title: Text("Tu coś będzie"),
+                                subtitle: Text("Może dostępni trenerzy"),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PageViewBox extends StatelessWidget {
+  PageViewBox({
+    super.key,
+    required List<Map<String, String>> dataList,
+  })  : _dataList = dataList;
+
+  final List<Map<String, String>> _dataList;
+  final PageController _pageController = PageController(
+    viewportFraction:
+    0.90, // Ustawienie viewportFraction na 0.8 (czyli 80% szerokości ekranu)
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: _dataList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            margin: EdgeInsets.symmetric(horizontal: 6.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 150,
+                  width: double.infinity,
+                  child: Image.network(
+                    _dataList[index]['image']!,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    _dataList[index]['nazwa']!,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Miasto: ${_dataList[index]['miasto']}'),
+                      Text('Ulica: ${_dataList[index]['ulica']}'),
+                      RichText(
+                        text: TextSpan(children: [
+                          TextSpan(
+                              text: "Otwarte dziś: ",
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .color)),
+                          TextSpan(
+                              text: _dataList[index]['hours'],
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .color))
+                        ]),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CardTraining extends StatefulWidget {
+  CardTraining({
+    super.key,
+  });
+
+  @override
+  State<CardTraining> createState() => _CardTrainingState();
+}
+
+class _CardTrainingState extends State<CardTraining> {
+  bool isTrainingCompleted = false; // Początkowa wartość Checkboxa
+  String trainingTime = "2:00h";
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                // Kolor tła kontenera
+                borderRadius: BorderRadius.circular(10),
+                // Zaokrąglenie rogów kontenera
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.1), // Kolor cienia
+                    spreadRadius: 3, // Rozprzestrzenienie cienia
+                    blurRadius: 7, // Rozmycie cienia
+                    offset: Offset(0, 3), // Offset cienia
+                  ),
+                ],
+              ),
+              child: CalendarWeek()),
+          ListTile(
+            title: Text(
+              "Trening klaty",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Szacowany czas treningu: " + trainingTime),
+                Text('Kliknij aby przejść do szczegółów'),
+              ],
+            ),
+            trailing: Checkbox(
+              value: isTrainingCompleted,
+              onChanged: (newValue) {
+                setState(() {
+                  isTrainingCompleted = newValue!;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -51,7 +290,10 @@ class HeaderDrawerItem extends StatelessWidget {
               top: 24 + MediaQuery.of(context).padding.top, bottom: 24),
           child: Column(
             children: [
-              CircleAvatar(radius: 52, backgroundImage: NetworkImage('https://cdn3.iconfinder.com/data/icons/design-n-code/100/272127c4-8d19-4bd3-bd22-2b75ce94ccb4-512.png')),
+              CircleAvatar(
+                  radius: 52,
+                  backgroundImage: NetworkImage(
+                      'https://cdn3.iconfinder.com/data/icons/design-n-code/100/272127c4-8d19-4bd3-bd22-2b75ce94ccb4-512.png')),
               _UserDetails(),
             ],
           ),
@@ -74,8 +316,14 @@ class _UserDetails extends StatelessWidget {
       padding: EdgeInsets.only(top: 10),
       child: Column(
         children: [
-          Text("John Doe", style: theme.bodyLarge,),
-          Text("JohnDoe@gmail.com", style: theme.bodyMedium,)
+          Text(
+            "John Doe",
+            style: theme.bodyLarge,
+          ),
+          Text(
+            "JohnDoe@gmail.com",
+            style: theme.bodyMedium,
+          )
         ],
       ),
     );
